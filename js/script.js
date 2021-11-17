b7945c7cceba1a1d64e066735a30343616e2ce06
renaatosalgado <renaato.salgado@gmail.com>
2021-11-17 17:14:39 -0300

    initial files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const URL_API = "https://mock-api.bootcamp.respondeai.com.br/api/v4/buzzquizz";
+const APP = document.querySelector(".app");
+
+//============== TELA 01 ==============//
+
+
+
+//============== TELA 02 ==============//
+
+
+
+//============== TELA 03 ==============//
+
+
+
+//============== AUX FUNCTIONS ==============//
